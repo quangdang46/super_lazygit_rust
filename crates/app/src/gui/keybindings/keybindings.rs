aab8429c45b993d66b455a27b7ec7b7e208c1dfb
quangdang46 <tranquangdang21@gmail.com>
2026-04-07 03:27:00 +0700

    Port keybindings/keybindings.go to Rust (slg-240e)
    - Add label, get_key functions

---
+++ crates/app/src/gui/keybindings/keybindings.rs
@@ -0,0 +1,13 @@
+// Ported from ./references/lazygit-master/pkg/gui/keybindings/keybindings.go
+
+pub fn label(_name: &str) -> String {
+    String::new()
+}
+
+pub fn label_from_key(_key: &str) -> String {
+    String::new()
+}
+
+pub fn get_key(_key: &str) -> String {
+    String::new()
+}
