7f95af5e3b8b464e72575b5ba407f18171ba34ea
quangdang46 <tranquangdang21@gmail.com>
2026-04-07 06:26:27 +0700

    feat(test-support): port delete_from_history test (slg-1c5q)
    Add delete_from_history integration test.

--- crates/test-support/src/integration_tests/shell_commands/mod.rs
+++ crates/test-support/src/integration_tests/shell_commands/mod.rs
@@ -1,9 +1,11 @@
 pub mod basic_shell_command;
+pub mod delete_from_history;
 pub mod edit_history;
 pub mod history;
 pub mod omit_from_history;
 
 pub use basic_shell_command::BASIC_SHELL_COMMAND;
+pub use delete_from_history::DELETE_FROM_HISTORY;
 pub use edit_history::EDIT_HISTORY;
 pub use history::HISTORY;
 pub use omit_from_history::OMIT_FROM_HISTORY;
