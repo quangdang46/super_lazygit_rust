f8a9806a3c4d9ad88a5630f7408550d912161929
quangdang46 <tranquangdang21@gmail.com>
2026-04-07 08:29:59 +0700

    Port 3 submodule integration test stubs (enter_nested, remove, reset)

---
+++ crates/test-support/src/integration_tests/submodule/enter_nested.rs
@@ -0,0 +1,23 @@
+use crate::integration_test::{IntegrationTest, NewIntegrationTestArgs};
+
+pub struct EnterNestedTest;
+
+impl EnterNestedTest {
+    pub fn new() -> IntegrationTest {
+        IntegrationTest::new(NewIntegrationTestArgs {
+            description: "Enter a nested submodule".to_string(),
+            extra_cmd_args: vec![],
+            skip: false,
+            setup_repo: Some(Box::new(|_shell| {})),
+            setup_config: Some(Box::new(|_cfg| {})),
+            run: Some(Box::new(|_t, _keys| {})),
+            extra_env_vars: std::collections::HashMap::new(),
+            git_version: Default::default(),
+            width: 0,
+            height: 0,
+            is_demo: false,
+        })
+    }
+}
+
+pub const ENTER_NESTED: EnterNestedTest = EnterNestedTest;
--- crates/test-support/src/integration_tests/submodule/mod.rs
+++ crates/test-support/src/integration_tests/submodule/mod.rs
@@ -1,5 +1,11 @@
 pub mod add;
 pub mod enter;
+pub mod enter_nested;
+pub mod remove;
+pub mod reset;
 
 pub use add::ADD;
 pub use enter::ENTER;
+pub use enter_nested::ENTER_NESTED;
+pub use remove::REMOVE;
+pub use reset::RESET;
