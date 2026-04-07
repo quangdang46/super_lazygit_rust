dda9ea181c08b0d423cc32dcb2c6c3c1cc558412
quangdang46 <tranquangdang21@gmail.com>
2026-04-07 09:25:01 +0700

    Port 3 interactive_rebase integration test stubs (move_commit, move_update_ref_todo, move_across_branch_boundary_outside_rebase)

--- crates/test-support/src/integration_tests/interactive_rebase/mod.rs
+++ crates/test-support/src/integration_tests/interactive_rebase/mod.rs
@@ -2,10 +2,16 @@ pub mod amend_first_commit;
 pub mod amend_fixup_commit;
 pub mod amend_merge;
 pub mod drop_todo_commit_with_update_ref;
+pub mod move_across_branch_boundary_outside_rebase;
+pub mod move_commit;
+pub mod move_update_ref_todo;
 pub mod quick_start;
 
 pub use amend_first_commit::AMEND_FIRST_COMMIT;
 pub use amend_fixup_commit::AMEND_FIXUP_COMMIT;
 pub use amend_merge::AMEND_MERGE;
 pub use drop_todo_commit_with_update_ref::DROP_TODO_COMMIT_WITH_UPDATE_REF;
+pub use move_across_branch_boundary_outside_rebase::MOVE_ACROSS_BRANCH_BOUNDARY_OUTSIDE_REBASE;
+pub use move_commit::MOVE_COMMIT;
+pub use move_update_ref_todo::MOVE_UPDATE_REF_TODO;
 pub use quick_start::QUICK_START;
